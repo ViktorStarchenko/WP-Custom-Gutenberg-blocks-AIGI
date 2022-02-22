73e84df71bcb9576a042716493edb3cd81dfb710
Viktor Starchenko <viktor.s@overdose.digital>
2022-02-22 13:02:51 +0200

    793159401 [AIGI]  FAQ's page - added button group to template. Created single column template

--- wp-content/themes/aigi/template-parts/content-blocks/content-custom-content.php
+++ wp-content/themes/aigi/template-parts/content-blocks/content-custom-content.php
@@ -439,7 +439,63 @@
                 </div>
             </div>
         
-    <?php endif ?>    
+    <?php endif ?>
+
+        <!--Buttons Group-->
+        <?php if ($content_item['item_type'] == 'Buttons Group') : ?>
+            <div class="content-item buttons-block">
+                <?php
+                $button_group = $content_item['button_group'];
+                $button_group_classes = '';
+                if ($button_group['alignment']['desktop']) {
+                    $button_group_classes .= ' ' . $button_group['alignment']['desktop'] . ' ';
+                }
+                if ($button_group['alignment']['mobile']) {
+                    $button_group_classes .= ' ' . $button_group['alignment']['mobile'] . ' ';
+                }
+
+                if ($button_group['margin']['margin_top']) {
+                    $button_group_classes .= ' ' . $button_group['margin']['margin_top'] . ' ';
+                }
+                if ($button_group['margin']['margin_bottom']) {
+                    $button_group_classes .= ' ' . $button_group['margin']['margin_bottom'] . ' ';
+                }
+
+                ?>
+
+                <?php if($button_group['buttons']) : ?>
+                    <div class="btn-group <?php echo $button_group_classes ; ?>">
+                        <?php foreach($button_group['buttons'] as $button) : ?>
+
+                            <?php
+                            $classes = '';
+                            if ($button['styling']['color']) {
+                                $classes .= ' ' . $button['styling']['color'] . ' ';
+                            }
+                            if ($button['styling']['icon']) {
+                                $classes .= ' ' . $button['styling']['icon'] . ' ';
+                            }
+                            if ($button['styling']['icon_position']) {
+                                $classes .= ' ' . $button['styling']['icon_position'] . ' ';
+                            }
+                            if ($button['styling']['alignment']) {
+                                $classes .= ' ' . $button['styling']['alignment'] . ' ';
+                            }
+                            if ($button['anchor'] == true) {
+                                $classes .= ' ' . 'crane' . ' ';
+                            }
+
+                            ?>
+                            <a href="<?php echo $button['button']['url'] ;?>" target="<?= $button['button']['target'] ?>" class="btn-<?php echo $button['id']; ?>  btn-body <?php echo $classes ?>">
+                                <span class="btn-inner"><?php echo $button['button']['title'] ;?></span>
+                            </a>
+                        <?php endforeach; ?>
+                        <?php wp_reset_postdata(); ?>
+                    </div>
+                <?php endif; //edif buttons ?>
+            </div>
+
+        <?php endif ?>
 
     <?php endforeach ?>
     <?php wp_reset_postdata(); ?>
