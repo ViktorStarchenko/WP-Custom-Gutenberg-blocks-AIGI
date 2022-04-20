c4c9d32775e06592058872587b82e9f6b1c046de
Viktor Starchenko <viktor.s@overdose.digital>
2022-04-20 20:16:53 +0300

    temporary updated download pdf button

--- wp-content/themes/aigi/template-parts/content-blocks/content-share-download.php
+++ wp-content/themes/aigi/template-parts/content-blocks/content-share-download.php
@@ -13,14 +13,50 @@
                 </a>
             </div>
         <?php endif ?>
+
+<!--        --><?php //if ($share_download['enable_download']) : ?>
+<!--            <div class="post-technical__item">-->
+<!--                <div class="post-technical__title">Download</div>-->
+<!--                <a class="post-technical__button" href="/pdf-test?post_id=--><?php //echo get_the_ID();?><!--"  target="_blank">-->
+<!--                    <img src="/wp-content/themes/aigi/assets/images/download-big.svg" alt="download">-->
+<!--                </a>-->
+<!--            </div>-->
+<!--        --><?php //endif ?>
+
         <?php if ($share_download['enable_download']) : ?>
             <div class="post-technical__item">
                 <div class="post-technical__title">Download</div>
-                <a class="post-technical__button" href="/pdf-test?post_id=<?php echo get_the_ID();?>"  target="_blank">
-                    <img src="/wp-content/themes/aigi/assets/images/download-big.svg" alt="download">
-                </a>
+                <div class="popup_item_wrapper" data-popup="">
+                    <div href="" class="popup_button post-technical__button">
+                        <img src="/wp-content/themes/aigi/assets/images/download-big.svg" alt="download">
+                    </div>
+
+                    <div class="popup-main-wrapper" id="popup-main-wrapper">
+                        <div class="item_popup_wrapper">
+                            <div class="popup_overlay"></div>
+                            <div class="popup_content_wrapper threatment">
+                                <div class="item_popup_content_inner">
+                                    <div class="popup_close_button"></div>
+                                    <div class="social-share__wrapper modal modal-content p-m">
+                                        <div class="social-share__header">
+                                            <div class="social-share__title">
+
+                                            </div>
+                                        </div>
+                                        <div class="social-share__container">
+                                            <?php echo do_shortcode('[gravityform id="8" title="false" description="false" ajax="true" tabindex="49"]'); ?>
+                                        </div>
+                                    </div>
+                                    <div class="popup_content_footer"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
             </div>
         <?php endif ?>
+
         <?php if ($share_download['enable_save']) : ?>
             <div class="post-technical__item">
                 <div class="post-technical__title">Save</div>
