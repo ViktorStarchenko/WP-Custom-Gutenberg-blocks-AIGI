1d87a4c5f931cddf91e8caf801d73898cd6b9af8
Viktor Starchenko <viktor.s@overdose.digital>
2022-04-22 12:07:10 +0300

    786435848 [AIGI] Section Download - updated pdf templates from string to ob_start()

--- wp-content/themes/aigi/template-parts/pdf-parts/pdf-people-info.php
+++ wp-content/themes/aigi/template-parts/pdf-parts/pdf-people-info.php
@@ -1,58 +1,41 @@
 <?php
 
-function get_pdf_people_position($post) {
-    $people_info_get_position = get_field('people_info',$post)['position'];
+$people_info_get_position = get_field('people_info',$post)['position'];
 
-    $html = '';
+if ($people_info_get_position) { ?>
+    <div class="post-details__item" style="margin:8px 0">
+        <div class="post-details__text"><?php echo $people_info_get_position; ?></div>
+    </div>
+<?php }
 
-    if ($people_info_get_position) {
-    $html.=    '<div class="post-details__item" style="margin:8px 0">';
-    $html.=        '<div class="post-details__text">'.$people_info_get_position.'</div>';
-    $html.=    '</div>';
-    }
 
-    return $html;
-}
+$people_info_get_qualification = get_field('people_info',$post);
 
-function get_pdf_people_qualification($post) {
-    $people_info_get_qualification = get_field('people_info',$post);
 
-    $html = '';
+if ($people_info_get_qualification['qualification']) { ?>
+    <div class="post-details__item">
+        <div class="post-details__text">
+            <ul class="rounded-list">
+            <?php foreach (get_field('people_info')['qualification'] as $qualification) { ?>
+                <li style="margin: 8px 0;"><?php echo $qualification["item"]; ?></li>
+    <?php } ?>
+            </ul>
+        </div>
+    </div>
+<?php }
 
-    if ($people_info_get_qualification['qualification']) {
-    $html .=    '<div class="post-details__item">';
-    $html .=        '<div class="post-details__text">';
-    $html .=            '<ul class="rounded-list">';
-                    foreach (get_field('people_info')['qualification'] as $qualification) {
-    $html .=                '<li style="margin: 8px 0;">'.$qualification["item"].'</li>';
-                    }
-    $html .=            '</ul>';
 
-    $html .=        '</div>';
+$people_info_get_topics_of_expertise = get_field('people_info',$post)['topics_of_expertise'];
 
-    $html .=    '</div>';
-    }
-
-    return $html;
-}
-
-function get_pdf_people_topics_of_expertis($post) {
-    $people_info_get_topics_of_expertise = get_field('people_info',$post)['topics_of_expertise'];
-
-    $html = '';
-
-    if ($people_info_get_topics_of_expertise) {
-    $html.=    '<div class="post-details__item border-top" style="margin: 16px;">';
-    $html.=        '<div class="post-details__heading topics_of_expertise" style="font-weight: bold;font-size: 19.98px;line-height: 22px;letter-spacing: 0.02em;color:#131032;margin-bottom: 16px;">topics of expertise:</div>';
-    $html.=        '<div class="post-details__text">';
-    $html.=            '<div class="post-tile__tags">';
-                     foreach ($people_info_get_topics_of_expertise as $topics_of_expertise) {
-    $html.=                 '<span class="content-tags__item small" style="background: #FFFFFF;border: 1px solid #e0e0e0;box-sizing: border-box;border-radius: 50px;padding: 4px 10px;display: inline-block;margin: 4px;font-family: Proxima Nova;font-style: normal;font-weight: bold;font-size: 11.85px;line-height: 19px;letter-spacing: 2px;text-transform: uppercase;color: #000000;text-decoration: none;">'.$topics_of_expertise["item"].'</span>';
-                     }
-    $html.=            '</div>';
-    $html.=        '</div>';
-    $html.=    '</div>';
-    }
-
-    return $html;
-}
+if ($people_info_get_topics_of_expertise) { ?>
+    <div class="post-details__item border-top" style="margin: 16px;">
+        <div class="post-details__heading topics_of_expertise" style="font-weight: bold;font-size: 19.98px;line-height: 22px;letter-spacing: 0.02em;color:#131032;margin-bottom: 16px;">topics of expertise:</div>
+            <div class="post-details__text">
+                <div class="post-tile__tags">
+                <?php foreach ($people_info_get_topics_of_expertise as $topics_of_expertise) { ?>
+                    <span class="content-tags__item small" style="background: #FFFFFF;border: 1px solid #e0e0e0;box-sizing: border-box;border-radius: 50px;padding: 4px 10px;display: inline-block;margin: 4px;font-family: Proxima Nova;font-style: normal;font-weight: bold;font-size: 11.85px;line-height: 19px;letter-spacing: 2px;text-transform: uppercase;color: #000000;text-decoration: none;"><?php echo $topics_of_expertise["item"]; ?></span>
+    <?php } ?>
+                </div>
+            </div>
+    </div>
+<?php }
