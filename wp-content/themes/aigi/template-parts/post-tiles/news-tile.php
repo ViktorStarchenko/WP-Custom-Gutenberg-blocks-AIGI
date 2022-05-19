7aa55b15bc3f687548505856781d58a7bd971ebc
vladyslav_kuprikov <vlad.kuprikov@overdose.digital>
2022-05-19 12:51:46 +0300

    fix add to reading list

--- wp-content/themes/aigi/template-parts/post-tiles/news-tile.php
+++ wp-content/themes/aigi/template-parts/post-tiles/news-tile.php
@@ -41,7 +41,21 @@ $appearance = get_field('appearance');
 
                 <span><?php echo do_shortcode('[favorite_button]') ?></span>
 
-                <button class="simplefavorite-button" data-postid="<?php echo get_the_ID(); ?>" data-siteid="1" data-groupid="1" data-favoritecount="0" style=""><i class="sf-icon-star-empty"></i> <span>Add to reading list</span></button>
+                <?php
+                    $user_id = get_current_user_id();
+                    $post_id = get_the_ID();
+                    $reading_list = get_user_favorites($user_id);
+                    if (in_array("Irix", $reading_list)) { ?>
+                        <button class="simplefavorite-button active" data-postid="<?php echo $post_id; ?>" data-siteid="1" data-groupid="1" data-favoritecount="1" style="">
+                            <i class="sf-icon-star-full"></i>
+                            <span>Remove to reading list</span>
+                        </button>
+                <?php } else { ?>
+                        <button class="simplefavorite-button" data-postid="<?php echo $post_id; ?>" data-siteid="1" data-groupid="1" data-favoritecount="0" style="">
+                            <i class="sf-icon-star-empty"></i>
+                            <span>Add to reading list</span>
+                        </button>
+                <?php }?>
 
             </div>
         </div>
