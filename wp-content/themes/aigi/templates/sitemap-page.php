e4df1c24e48c6c93b42e07b0533775c82ecae5b8
vladyslav_kuprikov <vlad.kuprikov@overdose.digital>
2022-06-02 16:02:09 +0300

    hotfix sitemap page

--- wp-content/themes/aigi/templates/sitemap-page.php
+++ wp-content/themes/aigi/templates/sitemap-page.php
@@ -17,17 +17,24 @@ $wpb_all_query = new WP_Query( [
     'order'   => 'DESC',
 ] );
 ?>
-<?php if ( $wpb_all_query->have_posts() ) : ?>
-    <ul>
-        <!-- the loop -->
-        <?php while ( $wpb_all_query->have_posts() ) : $wpb_all_query->the_post(); ?>
-            <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
-        <?php endwhile; ?>
-        <!-- end of the loop -->
-    </ul>
-    <?php wp_reset_postdata(); ?>
-<?php else : ?>
-    <p><?php _e( 'Извините, нет записей, соответствуюших Вашему запросу.' ); ?></p>
-<?php endif; ?>
+<div class="main-inner top-of-hero">
+    <div class="content-wrapper wrapper-1245">
+        <h1><?php echo get_the_title(); ?></h1>
+        <div class="post-tile__list landing-page">
+            <?php if ( $wpb_all_query->have_posts() ) : ?>
+                <ul>
+                    <!-- the loop -->
+                    <?php while ( $wpb_all_query->have_posts() ) : $wpb_all_query->the_post(); ?>
+                        <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+                    <?php endwhile; ?>
+                    <!-- end of the loop -->
+                </ul>
+                <?php wp_reset_postdata(); ?>
+            <?php else : ?>
+                <p><?php _e( 'Sorry, there is no results.' ); ?></p>
+            <?php endif; ?>
+        </div>
+    </div>
+</div>
 
 <?php get_footer();?>
