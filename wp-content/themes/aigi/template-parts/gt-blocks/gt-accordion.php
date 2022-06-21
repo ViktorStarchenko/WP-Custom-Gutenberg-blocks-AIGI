a13c5d1187ac0c56e2f485ba9b5033b25d337235
Viktor Starchenko <viktor.s@overdose.digital>
2022-06-21 21:54:52 +0300

    updated  custom gutenberg accordion block

--- wp-content/themes/aigi/template-parts/gt-blocks/gt-accordion.php
+++ wp-content/themes/aigi/template-parts/gt-blocks/gt-accordion.php
@@ -7,27 +7,29 @@
         $classname .= ' align' . $block['align'];
     }
 ?>
-
+<?php if (get_field('accordion_block')) { ?>
+    <?php $data = get_field('accordion_block'); ?>
 <div class="content-item accordion <?php echo esc_attr($classname)  ?>">
     <div class="accordion_wrapper">
-    <?php if (have_rows('accordion_block')) { ?>
-        <?php while (have_rows('accordion_block')) : the_row();  ?>
+
+        <?php foreach ($data as $item) {?>
             <div class="accordion_item">
-                <?php if(get_sub_field('content')): ?>
-                    <span class="title-h4 nav_list-title accordion_btn"><?php echo get_sub_field('title')?></span>
+                <?php if($item['content']): ?>
+                    <span class="title-h4 nav_list-title accordion_btn"><?php echo $item['title'];?></span>
                 <?php endif ?>
                 <div  class="accordion_panel">
-                    <?php if(get_sub_field('subtitle')): ?>
-                        <span class="accordion_subtitle"><?php echo get_sub_field('subtitle')?></span>
+                    <?php if($item['subtitle']): ?>
+                        <span class="accordion_subtitle"><?php echo $item['subtitle'];?></span>
                     <?php endif ?>
                     <div class="accordion_content">
-                        <?php if(get_sub_field('content')): ?>
-                            <?php echo get_sub_field('content')?>
+                        <?php if($item['content']): ?>
+                            <?php echo $item['content'];?>
                         <?php endif ?>
                     </div>
                 </div>
             </div>
-        <?php endwhile; ?>
-    <?php } ?>
+        <?php }?>
+
     </div>
 </div>
+<?php } ?>
